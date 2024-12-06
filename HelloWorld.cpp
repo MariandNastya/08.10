b7efb59cdb6f9d5ea85b97ef02952159423bdaf7
MariandNastya <malysevamariatta6@gmail.com>
2024-12-06 16:12:10 +0900

    Добавлены комментарии в код

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,13 +1,13 @@
-﻿#include <iostream>
-#include <string> 
+﻿#include <iostream> //библиотека для ввода-вывода
+#include <string> //библиотека для работы со строками
 
 int main() {
-    std::string name;
+    std::string name; //переменная для хранения имени
 
-    std::cout << "Введите ваше имя: ";
-    std::cin >> name;
+    std::cout << "Введите ваше имя: "; //ввод имени
+    std::cin >> name; //считывание введенного имени и его сохранение
 
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+    std::cout << "Hello world from " << name << std::endl; //вывод сообщения
+    return 0; //завершение программы
 }
 
