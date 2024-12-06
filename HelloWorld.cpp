98368a56a473fd28c8e78e88af508e42d3ce1ae7
MariandNastya <malysevamariatta6@gmail.com>
2024-12-06 13:54:37 +0900

    Добавление файла HelloWorld

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int x = 0;
+
+void print() {
+    cout << "Hello, World!" << endl;
+}
+
+int main() {
+    for (int i = 0; i < 1; i++) {
+        x++;
+    }
+    print();
+    return 0;
+}
